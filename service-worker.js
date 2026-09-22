0ab5e2a95092690e03ec5e7314e3e1a8fe98befb
Anatolii Brazhko <brazhkoanatolij@gmail.com>
2026-09-22 19:24:07 +0300

    style: compact packaging consumption entry

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "packaging-filling-hub-v0.8.42-1";
+const CACHE_NAME = "packaging-filling-hub-v0.8.43-1";
 const APP_SHELL = [
   "./",
   "./index.html",
--- src/config/app-config.js
+++ src/config/app-config.js
@@ -2,7 +2,7 @@ const runtimeConfig = globalThis.__PACKAGING_FILLING_CONFIG__ ?? {};
 
 export const APP_CONFIG = Object.freeze({
   name: "Packaging-Filling-Hub",
-  version: "0.8.42",
+  version: "0.8.43",
   locale: "ru-RU",
   timeZone: "Europe/Vilnius",
   refreshIntervalMs: 60_000,
